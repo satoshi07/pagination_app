daf95c303aed1892138ca64a6f2ce73ace782dad
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-22 15:19:50 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+
+  PER_PAGE = 10
+
   def index
-    @posts = Post.limit(10).order(created_at: :desc)
+    @posts = Post.page(params[:page]).per(PER_PAGE).order(created_at: :desc)
   end
 
   def create
