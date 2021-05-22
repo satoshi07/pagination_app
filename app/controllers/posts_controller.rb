110fa4f79481f3a24daeb6639f8c27cbaa51ce06
satoshi07 <satoshi.akaike7@gmail.com>
2021-05-22 16:18:28 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,15 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.page(params[:page]).per(PER_PAGE).order(created_at: :desc)
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to root_path
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
